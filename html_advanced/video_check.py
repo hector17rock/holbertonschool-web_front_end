5c4dfea4aee180414d27243278430ef2508ab12b
Hector Soto <hector_17rock@hotmail.com>
2025-07-08 01:40:54 -0400

    TASK38

--- html_advanced/video_check.py
+++ html_advanced/video_check.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
-
-with open('38-styleguide.html', 'r') as f:
-    content = f.read()
-
-url = ('https://intranet-projects-files.s3.amazonaws.com/webstack/'
-       'BigBuckBunny.mp4')
-if url in content:
-    print('video has correct source')
+with open('38-styleguide.html') as f:
+    if 'BigBuckBunny.mp4' in f.read():
+        print('video has correct source')
